e4ab933e8aaee47742b56dbb698043ae4c3468d9
Olivier Maisonneuve <olivierm@ex-situ.com>
2017-09-29 10:19:29 -0400

    Mocking SoapClient

---
+++ Tests/CreateMockTrait.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace EXS\CampaignerBundle\Tests;
+
+/**
+ * Trait CreateMockTrait
+ *
+ * @package EXS\CampaignerBundle\Tests
+ */
+trait CreateMockTrait
+{
+    /**
+     * @param string $class
+     * @param array  $calls
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     *
+     * @throws \Exception
+     */
+    public function doCreateMock($class, array $calls = array())
+    {
+        /** @var \PHPUnit_Framework_TestCase $this */
+        if (!$this instanceof \PHPUnit_Framework_TestCase) {
+            throw new \Exception(sprintf(
+                'Class "%s" does not extends "\PHPUnit_Framework_TestCase".',
+                get_class($this)
+            ));
+        }
+
+        $methods = $this->validateMockParameters($class, $calls);
+
+        $mockBuilder = $this->getMockBuilder($class);
+
+        if (interface_exists($class) === false) {
+            $mockBuilder
+                ->disableOriginalConstructor()
+                ->disableProxyingToOriginalMethods()
+            ;
+        }
+
+        $mock = $mockBuilder
+            ->setMethods($methods)
+            ->getMock()
+        ;
+
+        foreach ($calls as $index => $call) {
+            $mockCall = $mock
+                ->expects($this->at($index))
+                ->method($call['method'])
+            ;
+
+            if (isset($call['parameters'])) {
+                $methodWith = new \ReflectionMethod($mockCall, 'with');
+                $args = is_array($call['parameters']) ? $call['parameters'] : array($call['parameters']);
+
+                $that = $this;
+                $args = array_map(function ($arg) use ($that) {
+                    return $that->equalTo($arg);
+                }, $args);
+
+                $methodWith->invokeArgs($mockCall, $args);
+            }
+
+            if (isset($call['result'])) {
+                if ('__self' === $call['result']) {
+                    $mockCall->will($this->returnValue($mock));
+                } elseif ($call['result'] instanceof \Exception) {
+                    $mockCall->will($this->throwException($call['result']));
+                } else {
+                    $mockCall->will($this->returnValue($call['result']));
+                }
+            }
+        }
+
+        return $mock;
+    }
+
+    /**
+     * @param string $class
+     * @param array  $calls
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    private function validateMockParameters($class, array $calls)
+    {
+        if (
+            (!class_exists($class))
+            && (!interface_exists($class))
+        ) {
+            throw new \Exception(sprintf('Class "%s" does not exists.', $class));
+        }
+
+        $methods = array();
+        foreach ($calls as $call) {
+            if (!array_key_exists('method', $call)) {
+                throw new \Exception('Invalid $calls.');
+            }
+
+            if (!in_array($call['method'], $methods)) {
+                $methods[] = $call['method'];
+            }
+        }
+
+        return $methods;
+    }
+}
--- Tests/Service/AbstractSoapClientTest.php
+++ Tests/Service/AbstractSoapClientTest.php
@@ -3,11 +3,14 @@
 namespace EXS\CampaignerBundle\Tests\Service;
 
 use EXS\CampaignerBundle\Service\AbstractSoapClient;
+use EXS\CampaignerBundle\Tests\CreateMockTrait;
 
 class EmptyClient extends AbstractSoapClient { }
 
 class AbstractSoapClientTest extends \PHPUnit_Framework_TestCase
 {
+    use CreateMockTrait;
+
     public function testConstructor()
     {
         $client = new EmptyClient('https://ws.campaigner.com/2013/01/contactmanagement.asmx?WSDL', 'user', 'pass');
@@ -36,33 +39,37 @@ class AbstractSoapClientTest extends \PHPUnit_Framework_TestCase
 
     public function testCallMethod()
     {
-//        $responseHeaders = [];
-//
-//        $clientMock = $this->prophesize(\SoapClient::class);
-//        $clientMock->__soapCall(
-//            'SomeMethod',
-//            [
-//                'SomeMethod' => [
-//                    'authentication' => [
-//                        'Username' => 'user',
-//                        'Password' => 'pass',
-//                    ],
-//                    'foo' => 123,
-//                    'bar' => 'baz',
-//                ]
-//            ],
-//            null,
-//            null,
-//            $responseHeaders
-//        )->willReturn(['result' => 'test'])->shouldBeCalledTimes(1);
+        $responseHeaders = [];
+
+        $soapResult = new \stdClass();
+        $soapResult->test = 123;
+
+        $clientMock = $this->doCreateMock(\SoapClient::class, [
+            ['method' => '__soapCall', 'parameters' => [
+                'SomeMethod',
+                [
+                    'SomeMethod' => [
+                        'authentication' => [
+                            'Username' => 'user',
+                            'Password' => 'pass',
+                        ],
+                        'foo' => 123,
+                        'bar' => 'baz',
+                    ]
+                ],
+                null,
+                null,
+                $responseHeaders
+            ], 'result' => []],
+        ]);
 
         $client = new EmptyClient('https://ws.campaigner.com/2013/01/contactmanagement.asmx?WSDL', 'user', 'pass');
 
         $reflection = new \ReflectionClass($client);
 
-//        $property = $reflection->getProperty('client');
-//        $property->setAccessible(true);
-//        $property->setValue($client, $clientMock);
+        $property = $reflection->getProperty('client');
+        $property->setAccessible(true);
+        $property->setValue($client, $clientMock);
 
         $method = $reflection->getMethod('callMethod');
         $method->setAccessible(true);
